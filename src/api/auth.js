3f2d2046b66a1a1607bd598c323b24470a0fe9f9
B.S.N. Abhiram <abhirambsn@gmail.com>
2021-04-14 21:15:12 +0530

    Added dotenv support

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import env from 'react-dotenv'
 
 const authAPIHandler = axios.create({
-    baseURL: "http://localhost:5000/api/user"
+    baseURL:  env.API_URL + "/api/user"
 })
 
 export { authAPIHandler }
--- src/api/todo.js
+++ src/api/todo.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import env from 'react-dotenv'
 
 const todoAPIHandler = axios.create({
-    baseURL: "http://localhost:5000/api/todo"
+    baseURL: env.API_URL + "/api/todo"
 })
 
 export { todoAPIHandler }
